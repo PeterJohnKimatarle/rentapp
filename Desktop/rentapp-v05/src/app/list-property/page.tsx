3a714047b0684dd07b0ff5b3b9a588bac59a6400
Peter John Kimatarle <peterjohnkimatarle@gmail.com>
2025-12-21 12:58:12 +0300

    Change rental rate empty option from 'Select period' to '---'

--- Desktop/rentapp-v05/src/app/list-property/page.tsx
+++ Desktop/rentapp-v05/src/app/list-property/page.tsx
@@ -922,7 +922,7 @@ export default function ListPropertyPage() {
                       }}
                       className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
                     >
-                      <option value="" className="text-gray-800">Select period</option>
+                      <option value="" className="text-gray-800">---</option>
                       <option value="price-month" className="text-gray-800">Price/month</option>
                       <option value="price-night" className="text-gray-800">Price/night</option>
                       <option value="price-hour" className="text-gray-800">Price/hour</option>
